3c0fe1d37da71a93a5680032b8282839ae2d60ab
Per Storgaard Laursen <perl@easj.dk>
2017-04-29 20:17:42 +0200

    First batch

---
+++ Unsolved/ExamAdmV21/ExamAdmV21/Student.cs
@@ -0,0 +1,48 @@
+﻿namespace ExamAdmV21
+{
+    public class Student
+    {
+        private string _name;
+        private int _yearOfBirth;
+        private string _country;
+        private string _imageSource;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int YearOfBirth
+        {
+            get { return _yearOfBirth; }
+        }
+
+        public string Country
+        {
+            get { return _country; }
+        }
+
+        public string ImageSource
+        {
+            get { return _imageSource; }
+        }
+
+        public string CountryStr
+        {
+            get { return "From " + Country; }
+        }
+
+        public string BirthStr
+        {
+            get { return "(Born " + YearOfBirth + ")"; }
+        }
+
+        public Student(string name, int yearOfBirth, string country, string imageSource)
+        {
+            _name = name;
+            _yearOfBirth = yearOfBirth;
+            _country = country;
+            _imageSource = imageSource;
+        }
+    }
+}
---
+++ Unsolved/ExamAdmV21/ExamAdmV21/StudentItemViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ExamAdmV21
+{
+    public class StudentItemViewModel : INotifyPropertyChanged
+    {
+        private Student _domainObject;
+
+        public StudentItemViewModel(Student s)
+        {
+            _domainObject = s;
+        }
+
+        public string Name
+        {
+            get { return _domainObject.Name; }
+        }
+
+        public string ImageSource
+        {
+            get { return _domainObject.ImageSource; }
+        }
+
+        public string CountryStr
+        {
+            get { return "From " + _domainObject.Country; }
+        }
+
+        public string BirthStr
+        {
+            get { return "(Born " + _domainObject.YearOfBirth + ")"; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
