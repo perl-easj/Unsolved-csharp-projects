2fd0b3bf126fbabbc2a4b5c5dc41dc951c546871
Per Storgaard Laursen <perl@easj.dk>
2017-07-25 10:36:54 +0200

    Added exercise projects on delegates, event and LINQ
    Added exercise projects on delegates, event and LINQ

---
+++ Unsolved/LambdaAnimals/LambdaAnimals/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LambdaAnimals
+{
+    /// <summary>
+    /// This class is supposed to represent any kind of animal.
+    /// The type of the animal should be returned by the AnimalType property.
+    /// </summary>
+    public class Animal
+    {
+        private Func<string> _animalTypeFunc;
+        private Func<string> _soundFunc;
+
+        public Animal(Func<string> animalTypeFunc, Func<string> soundFunc)
+        {
+            _soundFunc = soundFunc;
+            _animalTypeFunc = animalTypeFunc;
+        }
+
+        /// <summary>
+        /// Returns the type of the animal, e.g. Cat, Dog or something else. 
+        /// </summary>
+        public string AnimalType
+        {
+            get { return _animalTypeFunc(); }
+        }
+
+        /// <summary>
+        /// Returns the sound this particular animal makes.
+        /// </summary>
+        public string Sound
+        {
+            get { return _soundFunc(); }
+        }
+
+        /// <summary>
+        /// Convenient ToString override...
+        /// </summary>
+        public override string ToString()
+        {
+            return "A " + AnimalType + " that says " + Sound;
+        }
+    }
+}
---
+++ Unsolved/LambdaAnimals/LambdaAnimals/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create some Animal objects
+
+
+            // Create a List<Animal>, and insert the Animal 
+            // objects into the List
+
+
+            // Print out all Animal objects (or those matching
+            // your selection criterion).
+
+
+            KeepConsoleWindowOpen();
+        }
+
+
+        private static void KeepConsoleWindowOpen()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close application");
+            Console.ReadKey();
+        }
+    }
+}
