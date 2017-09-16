320a1e9904e8eead331700b38cf527322a9ca069
Per Storgaard Laursen <perl@easj.dk>
2017-09-16 20:58:24 +0200

    Added CrossTalk and project for studie-start
    Added CrossTalk and project for studie-start

---
+++ Unsolved/StudieStartE17/StudieStartE17/InsertCodeHere.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StudieStartE17
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // Skattesatser
+            int skatLimit = 50000;
+            int skatProcentUnderLimit = 80;
+            int skatProcentOverLimit = 150;
+            int skatRabatPrKmOverBenzinLimit = 4000;
+            int benzinLimit = 16;
+
+            // Det kan i opgaveløsningen antages, at
+            // 1) Alle biler har en nettopris, som er højere end skatLimit
+            // 2) Alle biler kører mindst 16 km på literen
+
+            // Skal afprøves med disse cases:
+            // A) nettoPris = 60000, 19.5 km/liter  -> Total pris 101.000 kr.
+            // A) nettoPris = 110000, 25.0 km/liter -> Total pris 204.000 kr. 
+            // A) nettoPris = 170000, 17.5 km/liter -> Total pris 384.000 kr.
+
+            // Oplysninger om bilen
+            int nettoPris = 60000;
+            double kmPrLiter = 19.5;
+            double totalPris = 0;
+
+            // Her skal du indsætte kode, som beregner den korrekte værdi af totalPris
+
+            Console.WriteLine($"Total pris for bilen : {totalPris} kr.");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ Unsolved/StudieStartE17/StudieStartE17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudieStartE17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertCodeHere theCode = new InsertCodeHere();
+            theCode.MyCode();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close the program...");
+
+            Console.ReadKey();
+        }
+    }
+}
