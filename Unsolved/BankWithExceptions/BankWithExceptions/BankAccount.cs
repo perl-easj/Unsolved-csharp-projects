5a9478537680769f3d3adf65114c1fee49630a57
Per Storgaard Laursen <perl@easj.dk>
2017-11-03 07:50:51 +0100

    Added BankWithExceptions exercise project
    Added BankWithExceptions exercise project

---
+++ Unsolved/BankWithExceptions/BankWithExceptions/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿namespace BankWithExceptions
+{
+    class BankAccount
+    {
+        private double _balance;
+        private double _interestRate;
+
+        public BankAccount(double interestRate)
+        {
+            _interestRate = interestRate;
+            _balance = 0.0;
+        }
+
+        /// <summary>
+        /// Balance of the account; must not become negative
+        /// </summary>
+        public double Balance
+        {
+            get { return _balance; }
+        }
+
+        /// <summary>
+        /// Interest rate of the account; must be between 0.0 and 20.0
+        /// </summary>
+        public double InterestRate
+        {
+            get { return _interestRate; }
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance = _balance + amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (_balance < amount)
+            {
+                WithdrawAmountTooLargeException e = new WithdrawAmountTooLargeException($"Amount was {amount} kr., balance was {_balance} kr.");
+                throw e;
+            }
+
+            _balance = _balance - amount;
+        }
+    }
+}
