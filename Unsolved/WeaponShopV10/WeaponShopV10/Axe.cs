b58ee3b8ad0fc8e31754734dba95cf172ac8ff13
Per Storgaard Laursen <perl@easj.dk>
2018-04-04 18:34:06 +0200

    Added WeaponShopV10 and V20 projects
    Added WeaponShopV10 and V20 projects

---
+++ Unsolved/WeaponShopV10/WeaponShopV10/Axe.cs
@@ -0,0 +1,18 @@
+﻿namespace WeaponShopV10
+{
+    /// <summary>
+    /// This class represents a Axe. An Axe is 
+    /// considered to be a weapon.
+    /// </summary>
+    public class Axe : Weapon
+    {
+        public const int AxeMinDamage = 20;
+        public const int AxeMaxDamage = 50;
+
+        #region Constructor
+        public Axe(string description) : base(description, AxeMinDamage, AxeMaxDamage)
+        {
+        } 
+        #endregion
+    }
+}
---
+++ Unsolved/WeaponShopV10/WeaponShopV10/Wand.cs
@@ -0,0 +1,18 @@
+﻿namespace WeaponShopV10
+{
+    /// <summary>
+    /// This class represents a Wand. A Wand is 
+    /// considered to be a weapon.
+    /// </summary>
+    public class Wand : Weapon
+    {
+        public const int WandMinDamage = 10;
+        public const int WandMaxDamage = 30;
+
+        #region Constructor
+        public Wand(string description) : base(description, WandMinDamage, WandMaxDamage)
+        {
+        } 
+        #endregion
+    }
+}
